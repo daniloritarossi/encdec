762bf204646f9bf4d24f56e698cb48a9ee5dc4eb
Ritarossi Danilo <danilo.ritarossi@nttdata.com>
2022-01-03 09:25:49 +0100

    FIX:: comments in a file

--- main.go
+++ main.go
@@ -32,10 +32,10 @@ func init() {
 }
 
 func main() {
-	//InfoLogger.Println("Starting the application...")
-	//InfoLogger.Println("Something noteworthy happened")
-	//WarningLogger.Println("There is something you should know about")
-	//ErrorLogger.Println("Something went wrong")
+	// InfoLogger.Println("Starting the application...")
+	// InfoLogger.Println("Something noteworthy happened")
+	// WarningLogger.Println("There is something you should know about")
+	// ErrorLogger.Println("Something went wrong")
 
 	// Get Machine ID in var mId
 	mId, err := machineid.ID()
@@ -49,7 +49,7 @@ func main() {
 	// jh const
 	const j = "jY-1"
 
-	//Unique ID combination of mId (Machine ID) + myOS (IDOS)
+	// Unique ID combination of mId (Machine ID) + myOS (IDOS)
 	var uniqueId string = j + mId + myOS
 
 	// Create a key based on OS and machine ID es: windows7fd705af-1b77-42c0-9f00-42330d32e19d
@@ -82,57 +82,57 @@ func main() {
 }
 
 func encryptString(stringToEncrypt string, keyString string) (encryptedString string) {
-	//decode string to bytes
+	// decode string to bytes
 	key, _ := hex.DecodeString(keyString)
 	plaintext := []byte(stringToEncrypt)
 
-	//New Cipher Block created from the key
+	// New Cipher Block created from the key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//New GCM created
+	// New GCM created
 	aesGCM, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//Create a nonce. Nonce should be from GCM
+	// Create a nonce. Nonce should be from GCM
 	nonce := make([]byte, aesGCM.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
 
-	//Encrypt the data
+	// Encrypt the data
 	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
 	return fmt.Sprintf("%x", ciphertext)
 }
 
 func decryptString(encryptedString string, keyString string) (decryptedString string) {
-	//decode string to bytes
+	// decode string to bytes
 	key, _ := hex.DecodeString(keyString)
 	enc, _ := hex.DecodeString(encryptedString)
 
-	//New Cipher Block created from the key
+	// New Cipher Block created from the key
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//New GCM created
+	// New GCM created
 	aesGCM, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//Get the nonce size
+	// Get the nonce size
 	nonceSize := aesGCM.NonceSize()
 
-	//Extract the nonce from enc data
+	// Extract the nonce from enc data
 	nonce, ciphertext := enc[:nonceSize], enc[nonceSize:]
 
-	//DENC the data
+	// DENC the data
 	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		panic(err.Error())
